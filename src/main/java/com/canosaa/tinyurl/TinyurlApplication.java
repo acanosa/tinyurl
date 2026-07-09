40e02dd6c392a9d44e2639ac5fc21494116f2a8e
acanosa <ale.canosa19@gmail.com>
2026-07-08 22:44:22 -0300

    Added docker compose services and redis as cache

--- src/main/java/com/canosaa/tinyurl/TinyurlApplication.java
+++ src/main/java/com/canosaa/tinyurl/TinyurlApplication.java
@@ -3,10 +3,13 @@ package com.canosaa.tinyurl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class TinyurlApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(TinyurlApplication.class, args);
 	}
 
--- src/main/java/com/canosaa/tinyurl/service/DefaultUrlService.java
+++ src/main/java/com/canosaa/tinyurl/service/DefaultUrlService.java
@@ -5,6 +5,7 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -15,16 +16,26 @@ import java.util.Base64;
 @RequiredArgsConstructor
 public class DefaultUrlService implements UrlService {
 
-    private static final String SHORT_URL_DOMAIN = "http://tiny.acanosa";
+    private static final String LONG_URL_KEY = "longUrl:";
+    private static final String SHORT_URL_KEY = "shortUrl:";
     private static final long OFFSET = 1_000_000;
 
     @Autowired
     private final UrlRepository urlRepository;
     @Autowired
     private final EncoderService defaultEncoderService;
+    @Autowired
+    private final RedisTemplate<String, String> redisTemplate;
 
     @Override
     public String shortenUrl(String url) {
+        var redisKey = LONG_URL_KEY + url;
+        var cached =  redisTemplate.opsForValue().get(redisKey);
+        if (cached != null) {
+            System.out.println("Cache match in redis for key: " + redisKey);
+            return cached;
+        }
+
         var exists = urlRepository.existsByUrl(url);
         if (exists) {
             return urlRepository.findByUrl(url).get().getShortUrl();
@@ -33,17 +44,26 @@ public class DefaultUrlService implements UrlService {
                 .url(url)
                 .build();
         var saved = urlRepository.save(urlToSave);
+        System.out.println(saved.getId());
 
         var encoded = defaultEncoderService.toBase62(saved.getId() + OFFSET);
         saved.setShortUrl(encoded);
+        redisTemplate.opsForValue().set(redisKey, saved.getShortUrl());
         return encoded;
     }
 
     @Override
     public String getOriginalUrl(String shortUrl) {
-        String urlPath = shortUrl.replace(SHORT_URL_DOMAIN, "");
+        var redisKey = SHORT_URL_KEY + shortUrl;
+        var cachedValue = redisTemplate.opsForValue().get(redisKey);
+        if (cachedValue != null) {
+            System.out.println("Cache hit for redis with key: " + redisKey);
+            return cachedValue;
+        }
+
         var id = defaultEncoderService.fromBase62(shortUrl);
         var url = urlRepository.findById(id - OFFSET).orElseThrow(() -> new RuntimeException("Not found"));
+        redisTemplate.opsForValue().set(SHORT_URL_KEY + shortUrl, url.getUrl());
         return url.getUrl();
     }
 }
